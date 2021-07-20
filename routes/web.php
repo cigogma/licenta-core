282328078fb9903e565f82aead631c61ec0f083c
Cigogma <cigogma@github.com>
2021-07-20 15:17:54 +0200

    runtimes/routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (Request $request) {
+    dd($request->header());
     return view('welcome');
 });
